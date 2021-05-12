7e34688e05aa6070a5a02de5e0299c38766fcae5
Renner Borges <rennerferreira23@gmail.com>
2021-05-12 11:09:02 -0300

    feat: mostrando o triângulo de geração da sequência no exercício 03

--- Atividade 01/Exercicio03.java
+++ Atividade 01/Exercicio03.java
@@ -9,14 +9,35 @@ public class Exercicio03
 		System.out.println("Informe um número:");
 		int repeticoes = scanner.nextInt();
 
+		System.out.println("Deseja ver as etapas?");
+		boolean isViewTriangle = scanner.nextBoolean();
+
+		int views[] = new int[repeticoes];
+
 		int soma = 0;
 		int a = 0;
 		int b = 1;
 
+		System.out.println("\n");
 		for (int i = 0; i < repeticoes; i++){
 			a= b;
 			b= soma;
 			soma = a+b;
+
+			views[i] = soma;
+
+			if(isViewTriangle == true){
+				for(int view: views){
+					if(view != 0){
+						System.out.printf("%d", view);
+					}
+				}
+				System.out.println("");
+			}
+		}
+
+		if(isViewTriangle == true){
+			System.out.println("\n");
 		}
 
 		System.out.println("O resultado é " + soma);
