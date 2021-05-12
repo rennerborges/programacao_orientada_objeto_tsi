e89c9fb31403f4866d094c78acdc01907f65d4d5
Renner Borges <rennerferreira23@gmail.com>
2021-05-12 08:50:39 -0300

    feat: resolução do exercício 01

---
+++ Atividade 01/Exercicio01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exercicio01
+{
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Informe o valor da gasolina:");
+		float gasolinaValor = scanner.nextFloat();
+
+		System.out.println("Informe o valor do etanol:");
+		float etanolValor = scanner.nextFloat();
+
+		float calculo = etanolValor/gasolinaValor;
+
+		System.out.println("O resultado foi " + calculo);
+
+		if(calculo <= 0.5){
+			System.out.println("Abasteça com etanol");
+		}else{
+			System.out.println("Abasteça com gasolina");
+		}
+
+	}
+}
