a85abbdb62eec9ab2750126e4a3aa0ee5ad31890
Renner Borges <rennerferreira23@gmail.com>
2021-05-22 19:02:37 -0300

    feat: adicionando recursos a atividade 02

--- Atividade 02/Atividade02.java
+++ Atividade 02/Atividade02.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList;
 public class Atividade02
 {
 	public static void main(String[] args) {
-        ContaCorrente conta = new ContaCorrente();
+        ContaCorrente contaRenner = new ContaCorrente();
+		ContaCorrente contaMarcel = new ContaCorrente();
 
-		conta.depositar(50);
-		System.out.println("Saldo: R$ " + conta.saldo);
-		conta.sacar(150);
-		System.out.println("Saldo: R$ " + conta.saldo);
-		conta.transferir("Marcel", 12345678, 3210, 60, "Santander");
-		System.out.println("Saldo: R$ " + conta.saldo);
+		contaRenner.titular = "Renner Borges";
+		contaRenner.numeroConta = 3801654;
+		contaRenner.numeroAgencia = 5874;
+		contaRenner.banco = "NuBank";
+
+		contaMarcel.titular = "Marcel";
+		contaMarcel.numeroConta = 1035421;
+		contaMarcel.numeroAgencia = 2340;
+		contaMarcel.banco = "Bradesco";
+
+		contaRenner.depositar(250);
+		contaRenner.sacar(150);
+		contaRenner.transferir(contaMarcel, 100);
+		contaRenner.transferir(contaRenner, 100);
+
+
+
+
+		System.out.println("-----------------------Renner-----------------------");
+		contaRenner.consultarSaldo();
+		System.out.println("Historico: " + contaRenner.historico);
+		System.out.println("\n-----------------------Marcel-----------------------");
+		contaMarcel.consultarSaldo();
+		System.out.println("Historico: " + contaMarcel.historico);
+
+
+
 	}
 }
 
@@ -17,30 +40,49 @@ class ContaCorrente {
 	String titular;
 	int numeroConta;
 	int numeroAgencia;
-	float saldo;
+	private float saldo;
 	String banco;
+	ArrayList<String> historico = new ArrayList<String>();
 
 	void sacar(float valor){
 		if(this.isValidAction(this.saldo, valor)){
 			this.saldo -= valor;
 			this.alert("Sucesso","Saque realizado com sucesso, seu saldo atual R$ " + this.saldo);
+			this.historico.add("Saque com sucesso no valor de R$ " + valor);
 		}else{
-			this.alert("Alerta","Não foi possivel realizar o saque pois sua conta não possui o valor de R$ " + valor + ", seu saldo atual é de R$ " + this.saldo + ", seu saldo atual é de R$ " + this.saldo);
+			this.alert("Alerta","Não foi possivel realizar o saque pois sua conta não possui o valor de R$ " + valor + ", seu saldo atual é de R$ " + this.saldo);
+			this.historico.add("Saque não efetuado no valor de R$ " + valor);
 		}
 	}
 
 	void depositar(float valor){
 		this.saldo += valor;
 		this.alert("Sucesso","Deposito realizado com sucesso, seu saldo atual: R$ " + this.saldo);
+		this.historico.add("Deposito com sucesso no valor de R$ " + valor);
 	}
 
-	void transferir(String titular, int numeroConta, int numeroAgencia, float valor, String banco){
+	void transferir(ContaCorrente conta,float valor){
+		if(conta.titular == this.titular){
+			this.alert("Alerta","Não é possivel transferir para você mesmo!");
+			this.historico.add("Transferencia não efetuada para " + conta.titular + " no valor de R$ " + valor);
+			return;
+		}
+
 		if(this.isValidAction(this.saldo, valor)){
 			this.saldo -= valor;
-			this.alert("Sucesso"," Transferencia com o valor de R$ " + valor + " realizada com sucesso,\n para a conta de número " + numeroConta + " e agência " + numeroAgencia + " com o titular " + titular + " pertencente ao banco " + banco + ".\n Seu saldo atual é de R$ " + this.saldo);
+			conta.saldo += valor;
+			this.historico.add("Transferencia com sucesso para "+ conta.titular +" no valor de R$ " + valor);
+			conta.historico.add("Transferencia recebida de " + this.titular + " no valor de R$" + valor);
+
+			this.alert("Sucesso"," Transferencia com o valor de R$ " + valor + " realizada com sucesso,\n para a conta de número " + conta.numeroConta + " e agência " + conta.numeroAgencia + " com o titular " + conta.titular + " pertencente ao banco " + conta.banco + ".\n Seu saldo atual é de R$ " + this.saldo);
 		}else{
 			this.alert("Alerta","Não foi possivel realizar a transferencia pois sua conta não possui o valor de valor R$ " + valor + ", seu saldo atual é de R$ " + this.saldo);
+			this.historico.add("Transferencia não efetuada para " + this.titular + " no valor de R$ " + valor);
 		}
+	}
+
+	void consultarSaldo(){
+		System.out.println("Saldo: R$" + this.saldo);
 	}
 
 	boolean isValidAction(float saldo, float valorTransacao){
@@ -49,8 +91,8 @@ class ContaCorrente {
 	}
 
 	void alert(String titulo, String motivo){
-		System.out.println("----------------"+ titulo +"---------------");
+		System.out.println("\n----------------"+ titulo +"---------------");
 		System.out.println(motivo);
-		System.out.println("------------------------------------");
+		System.out.println("------------------------------------\n");
 	}
 }
