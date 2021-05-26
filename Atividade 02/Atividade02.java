17172eae39880beaa3c522babd324f6e511ca71f
Renner Borges <rennerferreira23@gmail.com>
2021-05-25 21:37:40 -0300

    feat: mudando logica na atividade02

--- Atividade 02/Atividade02.java
+++ Atividade 02/Atividade02.java
@@ -64,38 +64,25 @@ class ContaCorrente {
 			return false;
 		}
 		this.saldo += valor;
-		this.alert("Sucesso","Deposito realizado com sucesso, seu saldo atual: R$ " + this.saldo);
-		this.historico.add("Deposito com sucesso no valor de R$ " + valor);
+		this.alert("Sucesso","Deposito recebido com sucesso, seu saldo atual: R$ " + this.saldo);
+		this.historico.add("Deposito recebido com sucesso no valor de R$ " + valor);
 		return true;
 	}
 
 	boolean transferir(ContaCorrente conta,float valor){
-		if(valor <= 0){
-			this.alert("Alerta","Não foi é possivel transferir valores menores ou iguais a zero");
-			this.historico.add("Transferencia não efetuada para " + this.titular + " no valor de R$ " + valor);
-			return false;
-		}
 
 		if(conta.titular == this.titular){
 			this.alert("Alerta","Não é possivel transferir para você mesmo!");
-			this.historico.add("Transferencia não efetuada para " + conta.titular + " no valor de R$ " + valor);
+			this.historico.add("Transferencia não efetuada, não é possivel transferir para você mesmo.");
 			return false;
 		}
 
-		if(this.isValidAction(this.saldo, valor)){
-
-			this.saldo -= valor;
-			conta.saldo += valor;
-			this.historico.add("Transferencia com sucesso para "+ conta.titular +" no valor de R$ " + valor);
-			conta.historico.add("Transferencia recebida de " + this.titular + " no valor de R$" + valor);
-
+		if(sacar(valor)){
 			this.alert("Sucesso"," Transferencia com o valor de R$ " + valor + " realizada com sucesso,\n para a conta de número " + conta.numeroConta + " e agência " + conta.numeroAgencia + " com o titular " + conta.titular + " pertencente ao banco " + conta.banco + ".\n Seu saldo atual é de R$ " + this.saldo);
-			return true;
-		}else{
-			this.alert("Alerta","Não foi possivel realizar a transferencia pois sua conta não possui o valor de valor R$ " + valor + ", seu saldo atual é de R$ " + this.saldo);
-			this.historico.add("Transferencia não efetuada para " + this.titular + " no valor de R$ " + valor);
-			return false;
+			return conta.depositar(valor);
 		}
+
+		return false;
 	}
 
 	void consultarSaldo(){
