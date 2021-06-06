f1f65c3b0f12ab93cb074d98993c41bb450cab8c
Renner Borges <rennerferreira23@gmail.com>
2021-06-06 09:05:35 -0300

    feat: adicionando caixa

--- Atividade 02/Atividade02.java
+++ Atividade 02/Atividade02.java
@@ -1,21 +1,167 @@
-public class Atividade02
-{
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Atividade02 {
+	private static ArrayList<ContaCorrente> contasCorrentes;
+
 	public static void main(String[] args) {
-        ContaCorrente contaRenner = new ContaCorrente("Renner Borges", 3801654, 5874, "NuBank");
-		ContaCorrente contaMarcel = new ContaCorrente("Marcel", 1035421, 2340, "Bradesco");
+		byte operacao;
+		Scanner scanner = new Scanner(System.in);
 
-		contaRenner.depositar(250);
-		contaRenner.sacar(150);
-		contaRenner.transferir(contaMarcel, 100);
-		contaRenner.transferir(contaRenner, 100);
+		String titular;
+		int numeroConta;
+		int numeroAgencia;
+		String banco;
+		ContaCorrente conta;
+		float valor;
+		contasCorrentes = new ArrayList<ContaCorrente>();
 
+		do {
+			System.out.println("\n");
+			System.out.println("<<<<<<<<< Caixa >>>>>>>>");
+			System.out.println("1 - Criar uma nova conta");
+			System.out.println("2 - Exibir o saldo, ou extrato, de uma conta");
+			System.out.println("3 - Sacar um valor de uma conta");
+			System.out.println("4 - Depositar um valor em uma conta");
+			System.out.println("5 - Transferir valores de uma conta para outra");
+			System.out.println("6 - Sair");
 
-		System.out.println("-----------------------Renner-----------------------");
-		contaRenner.consultarSaldo();
-		System.out.println("Historico: " + contaRenner.historico);
-		System.out.println("\n-----------------------Marcel-----------------------");
-		contaMarcel.consultarSaldo();
-		System.out.println("Historico: " + contaMarcel.historico);
+			System.out.println("Informe a opção: ");
+			operacao = scanner.nextByte();
 
+			System.out.println("\n");
+
+			switch (operacao) {
+				case 1:
+					System.out.println("<<<<< Criar uma nova conta >>>>>");
+					System.out.println("\n");
+
+					System.out.println("Informe o titular:");
+					titular = scanner.next();
+
+					System.out.println("Informe o número da conta:");
+					numeroConta = scanner.nextInt();
+
+					System.out.println("Informe o número da agência:");
+					numeroAgencia = scanner.nextInt();
+
+					System.out.println("Informe o banco:");
+					banco = scanner.next();
+
+					conta = new ContaCorrente(titular, numeroConta, numeroAgencia, banco);
+					contasCorrentes.add(conta);
+
+					System.out.println("\n");
+					conta.extrato();
+					break;
+				case 2:
+					System.out.println("<<<<< Exibir o saldo, ou extrato, de uma conta >>>>>");
+					System.out.println("\n");
+
+					System.out.println("Informe o número da conta:");
+					numeroConta = scanner.nextInt();
+
+					conta = getContaByNumeroConta(numeroConta);
+
+					if (conta != null) {
+						conta.extrato();
+					} else {
+						System.out.println("Não conseguimos localizar uma conta com esse número " + numeroConta);
+					}
+
+					break;
+				case 3:
+					System.out.println("<<<<< Sacar um valor de uma conta >>>>>");
+					System.out.println("\n");
+
+					System.out.println("Informe o número da conta:");
+					numeroConta = scanner.nextInt();
+
+					conta = getContaByNumeroConta(numeroConta);
+
+					if (conta != null) {
+						System.out.println("Informe o valor do saque:");
+						valor = scanner.nextFloat();
+						conta.sacar(valor);
+					} else {
+						System.out.println("Não conseguimos localizar uma conta com esse número " + numeroConta);
+					}
+					break;
+				case 4:
+					System.out.println("<<<<< Depositar um valor em uma conta >>>>>");
+					System.out.println("\n");
+
+					System.out.println("Informe o número da conta:");
+					numeroConta = scanner.nextInt();
+
+					conta = getContaByNumeroConta(numeroConta);
+
+					if (conta != null) {
+						System.out.println("Informe o valor do deposito:");
+						valor = scanner.nextFloat();
+						conta.depositar(valor);
+					} else {
+						System.out.println("Não conseguimos localizar uma conta com esse número " + numeroConta);
+					}
+					break;
+				case 5:
+					System.out.println("<<<<< Transferir valores de uma conta para outra >>>>>");
+					System.out.println("\n");
+
+					System.out.println("Informe o número da conta origem:");
+					int numeroContaOrigem = scanner.nextInt();
+
+					ContaCorrente contaOrigem = getContaByNumeroConta(numeroContaOrigem);
+
+					if (contaOrigem == null) {
+						System.out.println(
+								"Não conseguimos localizar a conta origem, com esse número " + numeroContaOrigem);
+						break;
+					}
+
+					System.out.println("Informe o número da conta destino:");
+					int numeroContaDestino = scanner.nextInt();
+					ContaCorrente contaDestino = getContaByNumeroConta(numeroContaDestino);
+
+					if (contaDestino == null) {
+						System.out.println(
+								"Não conseguimos localizar a conta destino, com esse número " + numeroContaDestino);
+						break;
+					}
+
+					System.out.println("Informe o valor do deposito:");
+					valor = scanner.nextFloat();
+
+					Boolean isTransactionValid = false;
+
+					if (contaOrigem.sacar(valor)) {
+						isTransactionValid = contaDestino.depositar(valor);
+					}
+
+					if (isTransactionValid) {
+						System.out.println("Transferência efetuada com sucesso");
+					} else {
+						System.out.println("Transferência não efetuada");
+
+					}
+					break;
+				default:
+					break;
+			}
+
+		} while (operacao != 6);
+
+		scanner.close();
 	}
+
+	public static ContaCorrente getContaByNumeroConta(int numeroConta) {
+		for (ContaCorrente conta : contasCorrentes) {
+			if (conta.getNumeroConta() == numeroConta) {
+				return conta;
+			}
+		}
+
+		return null;
+	}
+
 }
--- Atividade 02/ContaCorrente.java
+++ Atividade 02/ContaCorrente.java
@@ -9,69 +9,84 @@ public class ContaCorrente {
 	private String banco;
 	public ArrayList<String> historico;
 
-    public ContaCorrente(String titular, int numeroConta, int numeroAgencia, String banco){
-        this.titular = titular;
-        this.numeroConta = numeroConta;
-        this.numeroAgencia = numeroAgencia;
-        this.banco = banco;
-        this.historico = new ArrayList<String>();
-        this.saldo = 0;
-    }
+	public ContaCorrente(String titular, int numeroConta, int numeroAgencia, String banco) {
+		this.titular = titular;
+		this.numeroConta = numeroConta;
+		this.numeroAgencia = numeroAgencia;
+		this.banco = banco;
+		this.historico = new ArrayList<String>();
+		this.saldo = 0;
+	}
 
-	public boolean sacar(float valor){
-		if(this.isValidAction(this.saldo, valor)){
+	public int getNumeroConta() {
+		return this.numeroConta;
+	}
+
+	public boolean sacar(float valor) {
+		if (this.isValidAction(this.saldo, valor)) {
 			this.saldo -= valor;
-			this.alert("Sucesso","Saque realizado com sucesso, seu saldo atual R$ " + this.saldo);
+			this.alert("Sucesso", "Saque realizado com sucesso, seu saldo atual R$ " + this.saldo);
 			this.historico.add("Saque com sucesso no valor de R$ " + valor);
 			return true;
-		}else{
-			this.alert("Alerta","Não foi possivel realizar o saque pois sua conta não possui o valor de R$ " + valor + ", seu saldo atual é de R$ " + this.saldo);
+		} else {
+			this.alert("Alerta", "Não foi possivel realizar o saque pois sua conta não possui o valor de R$ " + valor
+					+ ", seu saldo atual é de R$ " + this.saldo);
 			this.historico.add("Saque não efetuado no valor de R$ " + valor);
 			return false;
 		}
 	}
 
-	public boolean depositar(float valor){
-		if(valor < 0){
-			this.alert("Alerta","Não é possivel depositar valores menores que zero.");
+	public boolean depositar(float valor) {
+		if (valor < 0) {
+			this.alert("Alerta", "Não é possivel depositar valores menores que zero.");
 			this.historico.add("Deposito não efetuado no valo de R$ " + valor);
 			return false;
 		}
 		this.saldo += valor;
-		this.alert("Sucesso","Deposito recebido com sucesso, seu saldo atual: R$ " + this.saldo);
+		this.alert("Sucesso", "Deposito recebido com sucesso, seu saldo atual: R$ " + this.saldo);
 		this.historico.add("Deposito recebido com sucesso no valor de R$ " + valor);
 		return true;
 	}
 
-	public boolean transferir(ContaCorrente conta,float valor){
+	public boolean transferir(ContaCorrente conta, float valor) {
 
-		if(conta.titular == this.titular){
-			this.alert("Alerta","Não é possivel transferir para você mesmo!");
+		if (conta.titular == this.titular) {
+			this.alert("Alerta", "Não é possivel transferir para você mesmo!");
 			this.historico.add("Transferencia não efetuada, não é possivel transferir para você mesmo.");
 			return false;
 		}
 
-		if(sacar(valor)){
-			this.alert("Sucesso"," Transferencia com o valor de R$ " + valor + " realizada com sucesso,\n para a conta de número " + conta.numeroConta + " e agência " + conta.numeroAgencia + " com o titular " + conta.titular + " pertencente ao banco " + conta.banco + ".\n Seu saldo atual é de R$ " + this.saldo);
+		if (sacar(valor)) {
+			this.alert("Sucesso",
+					" Transferencia com o valor de R$ " + valor + " realizada com sucesso,\n para a conta de número "
+							+ conta.numeroConta + " e agência " + conta.numeroAgencia + " com o titular "
+							+ conta.titular + " pertencente ao banco " + conta.banco + ".\n Seu saldo atual é de R$ "
+							+ this.saldo);
 			return conta.depositar(valor);
 		}
 
 		return false;
 	}
 
-	public void consultarSaldo(){
-		System.out.println("Saldo: R$" + this.saldo);
+	public void extrato() {
+		System.out.println("<<<<< Extrato >>>>>");
+		System.out.println("\n");
+		System.out.println("Titular:" + this.titular);
+		System.out.println("Número conta:" + this.numeroConta);
+		System.out.println("Número agência: " + this.numeroAgencia);
+		System.out.println("Banco: " + this.banco);
+		System.out.println("Histórico: " + this.historico);
+		System.out.println("Saldo: R$ " + this.saldo);
 	}
 
-	private boolean isValidAction(float saldo, float valorTransacao){
+	private boolean isValidAction(float saldo, float valorTransacao) {
 		float restante = saldo - valorTransacao;
 		return restante >= 0 && valorTransacao > 0;
 	}
 
-	public void alert(String titulo, String motivo){
-		System.out.println("\n----------------"+ titulo +"---------------");
+	public void alert(String titulo, String motivo) {
+		System.out.println("\n----------------" + titulo + "---------------");
 		System.out.println(motivo);
 		System.out.println("------------------------------------\n");
 	}
 }
-
